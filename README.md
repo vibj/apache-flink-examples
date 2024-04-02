f553133cd01a7f96031f7fa62d79c71267e63e7a
vibj <biju.vates@gmail.com>
2024-04-02 23:10:43 +1100

    word count example

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 Working examples of apache flink
+
+1 . Creating a project 
+
+```
+mvn archetype:generate -DarchetypeGroupId=org.apache.flink \
+    -DarchetypeArtifactId=flink-quickstart-java \
+    -DarchetypeVersion=1.19.0 \
+    -DgroupId=stream-examples \
+    -DartifactId=word-count-example \
+    -Dversion=0.1 \
+    -Dpackage=org.cleverstep \
+    -DinteractiveMode=false
+```
---
+++ word-count-example/src/main/java/org/cleverstep/SocketWindowWordCount.java
@@ -0,0 +1,98 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cleverstep;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+/**
+ * Skeleton for a Flink DataStream Job.
+ *
+ * <p>For a tutorial how to write a Flink application, check the
+ * tutorials and examples on the <a href="https://flink.apache.org">Flink Website</a>.
+ *
+ * <p>To package your application into a JAR file for execution, run
+ * 'mvn clean package' on the command line.
+ *
+ * <p>If you change the name of the main class (with the public static void main(String[] args))
+ * method, change the respective entry in the POM.xml file (simply search for 'mainClass').
+ */
+public class SocketWindowWordCount {
+
+	public static void main(String[] args) throws Exception {
+
+		final int port = 9000;
+		// get the execution environment
+		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+		env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime);
+
+		// get input data by connecting to the socket
+		DataStream<String> text = env.socketTextStream("jobmanager", port, "\n");
+
+		// parse the data, group it, window it, and aggregate the counts
+		DataStream<WordWithCount> windowCounts = text
+				.flatMap(new FlatMapFunction<String, WordWithCount>() {
+					@Override
+					public void flatMap(String value, Collector<WordWithCount> out) {
+						for (String word : value.split("\\s")) {
+							out.collect(new WordWithCount(word, 1L));
+						}
+					}
+				})
+				.keyBy("word")
+				.timeWindow(Time.seconds(5), Time.seconds(1))
+				.reduce(new ReduceFunction<WordWithCount>() {
+					@Override
+					public WordWithCount reduce(WordWithCount a, WordWithCount b) {
+						return new WordWithCount(a.word, a.count + b.count);
+					}
+				});
+
+		// print the results with a single thread, rather than in parallel
+		windowCounts.print().setParallelism(1);
+
+		env.execute("Socket Window WordCount");
+	}
+
+	// Data type for words with count
+	public static class WordWithCount {
+
+		public String word;
+		public long count;
+
+		public WordWithCount() {}
+
+		public WordWithCount(String word, long count) {
+			this.word = word;
+			this.count = count;
+		}
+
+		@Override
+		public String toString() {
+			return word + " : " + count;
+		}
+	}
+}
